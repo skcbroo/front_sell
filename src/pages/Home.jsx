cef500e5a5750576ce5f1143b4f020efb627eec5
skcbroo <skcbroo@gmail.com>
2025-08-20 00:17:55 -0300

    Update Home.jsx

--- src/pages/Home.jsx
+++ src/pages/Home.jsx
@@ -149,13 +149,13 @@ export default function Home() {
                     {/* LOGO no canto superior-direito (maior e um pouco mais à esquerda) */}
                     <div
                         className="
-    absolute top-4 right-8
+    absolute top-4 right-12
     rounded-lg bg-white/95 backdrop-blur
     px-3 py-2 shadow ring-1 ring-black/5
   "
                     >
                         <img
-                            src="/logonova.png"
+                            src="/icon.png"
                             alt="MIDLEJ Capital"
                             className="h-12 md:h-14 w-auto select-none pointer-events-none drop-shadow-sm"
                         />
