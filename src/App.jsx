d3533a4f56f417a6262bc5971f3166f26c4c2447
skcbroo <skcbroo@gmail.com>
2026-02-27 11:45:44 -0300

    Update App.jsx

--- src/App.jsx
+++ src/App.jsx
@@ -1,6 +1,7 @@
 ﻿//import './App.css'; // ✅ Certifique-se que isso esteja no topo
 import { BrowserRouter, Routes, Route } from "react-router-dom";
 import Home from "./pages/Home";
+import ChatPage from './pages/ChatPage';
 
 function App() {
     return (
@@ -8,13 +9,14 @@ function App() {
             <Routes>
               
                 <Route path="/" element={<Home />} />
-              
+               <Route path="/chat" element={<ChatPage />} />
             </Routes>
         </BrowserRouter>
     );
 }
 
 export default App;
+
 
 
 
